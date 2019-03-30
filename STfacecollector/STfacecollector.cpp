dcd7b65e0e25f59d6e9f6226d850eb50a3aef474
HowsonLiu <948470636@qq.com>
2019-03-30 17:48:59 +0800

    face information collector test

--- STfacecollector/STfacecollector.cpp
+++ STfacecollector/STfacecollector.cpp
@@ -3,6 +3,8 @@
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
 
 #pragma comment(lib, "STfacedetection.lib")
 extern "C" __declspec(dllexport) bool ClassifierEmpty();
@@ -13,18 +15,21 @@ extern "C" __declspec(dllexport) bool ClassifierDetect(int rows, int cols, int t
 
 int main()
 {
-	int i;
-	std::cout << ClassifierEmpty();
-	std::cin >> i;
+	cv::VideoCapture cap(0);
+	if (!cap.isOpened()) return -1;
+	std::vector<cv::Rect>* pFaceRectVec;
+	if (!ClassifierEmpty()) return -1;
+	cv::Mat frame;
+	while (true) {
+		cap >> frame;
+		cv::imshow("face collector", frame);
+		cv::waitKey(30);
+		ClassifierDetect(frame.rows, frame.cols, frame.type(), frame.data, CV_HAAR_SCALE_IMAGE, 100, 100, 600, 600);
+		pFaceRectVec = static_cast<std::vector<cv::Rect>*> (GetVectorRect());
+		if (pFaceRectVec) {
+			std::cout << pFaceRectVec->size() << std::endl;
+			if(pFaceRectVec->size() > 0) break;
+		}
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
