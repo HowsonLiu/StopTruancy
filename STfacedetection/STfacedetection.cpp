1dca68d3da2c54c3925fd8043d093341d3426ccd
HowsonLiu <948470636@qq.com>
2019-04-01 12:13:36 +0800

    add a ex function

--- STfacedetection/STfacedetection.cpp
+++ STfacedetection/STfacedetection.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/face.hpp>
 #include <opencv.hpp>
 
-cv::CascadeClassifier g_cascade("./haarcascade_frontalface_alt");	// 默认采用haar的人脸集
+cv::CascadeClassifier g_cascade("D:\\Programming_Project\\OpenCV\\StopTruancy\\x64\\Debug\\haarcascade_frontalface_alt.xml");	// 默认采用haar的人脸集
 std::vector<cv::Rect> g_faceRect;
 cv::Mat g_gray;
 
@@ -27,6 +27,15 @@ bool ClassifierDetect(int rows, int cols, int type, void* data,
 	int flag, int minx, int miny, int maxx, int maxy) {
 	if (g_cascade.empty() || !data || rows <= 0 || cols <= 0) return false;
 	cv::Mat origin(rows, cols, type, data);
+	cv::cvtColor(origin, g_gray, CV_BGR2GRAY);	// 转灰度图
+	cv::equalizeHist(g_gray, g_gray);	// 直方图均衡化，简单点来说就是提高对比度
+	g_cascade.detectMultiScale(g_gray, g_faceRect, 1.1f, 3, flag, cv::Size(minx, miny), cv::Size(maxx, maxy));
+	return true;
+}
+
+bool ClassifierDetectEx(void* pMat, int flag, int minx, int miny, int maxx, int maxy) {
+	if (g_cascade.empty() || !pMat) return false;
+	cv::Mat origin = static_cast<cv::Mat*>(pMat)->clone();
 	cv::cvtColor(origin, g_gray, CV_BGR2GRAY);	// 转灰度图
 	cv::equalizeHist(g_gray, g_gray);	// 直方图均衡化，简单点来说就是提高对比度
 	g_cascade.detectMultiScale(g_gray, g_faceRect, 1.1f, 3, flag, cv::Size(minx, miny), cv::Size(maxx, maxy));
--- STfacedetection/STfacedetection.h
+++ STfacedetection/STfacedetection.h
@@ -8,3 +8,5 @@ extern "C" __declspec(dllexport) void* GetVectorRect();
 
 extern "C" __declspec(dllexport) bool ClassifierDetect(int rows, int cols, int type, void* data,
 	int flag, int minx, int miny, int maxx, int maxy);
+
+extern "C" __declspec(dllexport) bool ClassifierDetectEx(void* pMat, int flag, int minx, int miny, int maxx, int maxy);
