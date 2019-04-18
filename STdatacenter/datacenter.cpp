5762a46f37cdf0040a41e0869a16d67730ba17ec
HowsonLiu <948470636@qq.com>
2019-04-18 21:01:28 +0800

    data center

--- STdatacenter/datacenter.cpp
+++ STdatacenter/datacenter.cpp
@@ -1,12 +1,67 @@
 #include "datacenter.h"
+#include <QFile>
+#include <QTextStream>
 
-
-
-DataCenter::DataCenter()
+DataCenter::DataCenter() 
+	: m_rootPath(ROOT_PATH)
+	, m_allClassPath(m_rootPath + "\\class.stc")
+	, m_allStudentPath(m_rootPath + "\\student.sts")
 {
 }
 
 
 DataCenter::~DataCenter()
 {
+}
+
+DataCenter * DataCenter::getInstance()
+{
+	static DataCenter instance;
+	return &instance;
+}
+
+std::vector<QString> DataCenter::getAllClassName() const
+{
+	std::vector<QString> res;
+	QFile allClassFile(m_allClassPath);
+	if (allClassFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		while (!allClassFile.atEnd()) {
+			res.push_back(allClassFile.readLine());
+		}
+	}
+	allClassFile.close();
+	return res;
+}
+
+bool DataCenter::addClassName(const QString& name)
+{
+	QFile allClassFile(m_allClassPath);
+	if (!allClassFile.open(QIODevice::ReadWrite | QIODevice::Append | QIODevice::Text)) return false;
+	QTextStream writer(&allClassFile);
+	writer << name << endl;
+	allClassFile.close();
+	return false;
+}
+
+std::vector<QString> DataCenter::getAllStudentName() const
+{
+	std::vector<QString> res;
+	QFile allStudentFile(m_allStudentPath);
+	if (allStudentFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		while (!allStudentFile.atEnd()) {
+			res.push_back(allStudentFile.readLine());
+		}
+	}
+	allStudentFile.close();
+	return res;
+}
+
+bool DataCenter::addStudentName(const QString &name)
+{
+	QFile allStudentFile(m_allStudentPath);
+	if (!allStudentFile.open(QIODevice::ReadWrite | QIODevice::Append | QIODevice::Text)) return false;
+	QTextStream writer(&allStudentFile);
+	writer << name << endl;
+	allStudentFile.close();
+	return false;
 }
--- STdatacenter/datacenter.h
+++ STdatacenter/datacenter.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <QString>
+#include <vector>
+
+#define ROOT_PATH "D:\\tmp\\data"
+
 class DataCenter
 {
-public:
+private:
+	const QString m_rootPath;
+	const QString m_allClassPath;
+	const QString m_allStudentPath;
+
+private:
 	DataCenter();
 	~DataCenter();
-};
 
+public:
+	static DataCenter* getInstance();
+	std::vector<QString> getAllClassName() const;
+	bool addClassName(const QString& name);
+	std::vector<QString> getAllStudentName() const;
+	bool addStudentName(const QString& name);
+};
+#define DATA_CENTER_INSTANCE DataCenter::getInstance()
