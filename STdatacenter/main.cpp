1607b446527f1718ac4e45ab061c7f3d1ad53d3e
HowsonLiu <948470636@qq.com>
2019-04-21 16:07:11 +0800

    finish sqlitehelper

--- STdatacenter/main.cpp
+++ STdatacenter/main.cpp
@@ -31,6 +31,17 @@ int main(int argc, char *argv[])
 	SQLiteHelper h("D:\\tmp\\data\\class\\Math\\Math.db");
 	bool res;
 	if (h.isValid()) res = h.Init();
-	res = h.AddCols({ "3115005490", "3115005491"});
+	res = h.AddCol("3115005489");
+	res = h.AddCol("3115005490");
+	res = h.AddCol("3115005491");
+	res = h.AddCol("3115005492");
+	res = h.AddCol("3115005493");
+	res = h.AddRecord("2019-1-1", { "3115005489", "3115005490" });
+	res = h.AddRecord("2019-1-2", { "3115005491", "3115005492" });
+	res = h.AddRecord("2019-1-3", { "3115005489", "3115005492" });
+	res = h.AddRecord("2019-1-4", { "3115005490" });
+	int allnum, num;
+	res = h.GetRecordNums(&allnum);
+	res = h.GetStudentAttendanceNums("3115005489", &num);
 	return a.exec();
 }
--- STdatacenter/sqlitehelper.cpp
+++ STdatacenter/sqlitehelper.cpp
@@ -1,6 +1,19 @@
 #include "sqlitehelper.h"
 #include <sqlite3.h>
 
+/**
+ *@param:data 
+ *@param:argc number of columns
+ *@param:argv data of columns
+ *@param:azColName name of columns
+ *@return:sqlite3_exec function result
+ */
+int SQLiteHelper::CountCallBack(void* data, int argc, char** argv, char** azColName)
+{
+	if (argc <= 0) return SQLITE_ERROR;
+	*static_cast<int*>(data) = QString(argv[0]).toInt();
+	return SQLITE_OK;
+}
 
 SQLiteHelper::SQLiteHelper(const QString& path)
 	: m_bValid(true)
@@ -22,9 +35,41 @@ bool SQLiteHelper::Init()
 
 bool SQLiteHelper::AddCol(const QString& col)
 {
-	QString sql = "ALTER TABLE LESSON ADD COLUMN %1 INT;";	// sqlite没有bool
+	QString sql = "ALTER TABLE LESSON ADD COLUMN %1 INT DEFAULT 0;";	// sqlite没有bool
 	sql = sql.arg(StuName2Col(col));
 	return sqlite3_exec(m_db, sql.toUtf8(), nullptr, nullptr, nullptr) == SQLITE_OK;
+}
+
+bool SQLiteHelper::AddRecord(const QString& lesson, const std::vector<QString>& students)
+{
+	QString sql;
+	if (!students.size()) {
+		sql = QString("INSERT INTO LESSON(NAME) VALUES ('%1');").arg(lesson);
+	}
+	else {
+		sql = "INSERT INTO LESSON(NAME, %1) VALUES ('%2', %3);";
+		QString cols, values;
+		for (int i = 0; i < students.size() - 1; ++i) {
+			cols += StuName2Col(students[i]) + ", ";
+			values += "1, ";
+		}
+		cols += StuName2Col(students[students.size() - 1]);
+		values += "1";
+		sql = sql.arg(cols, lesson, values);
+	}
+	return sqlite3_exec(m_db, sql.toUtf8(), nullptr, nullptr, nullptr) == SQLITE_OK;
+}
+
+bool SQLiteHelper::GetRecordNums(int* num) const
+{
+	QString sql = "SELECT COUNT(*) FROM LESSON";
+	return sqlite3_exec(m_db, sql.toUtf8(), &CountCallBack, static_cast<void*>(num), nullptr) == SQLITE_OK;
+}
+
+bool SQLiteHelper::GetStudentAttendanceNums(const QString& name, int* num)
+{
+	QString sql = QString("SELECT COUNT(*) FROM LESSON WHERE %1 <> 0").arg(StuName2Col(name));
+	return sqlite3_exec(m_db, sql.toUtf8(), &CountCallBack, static_cast<void*>(num), nullptr) == SQLITE_OK;
 }
 
 QString StuName2Col(const QString& name) {
